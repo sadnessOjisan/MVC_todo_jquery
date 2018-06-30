cfdb434c53c10c157f8eae065e2450c06c5b7df8
sadness_ojisan <sadness.ojisan@gmail.com>
2018-06-30 15:13:32 +0900

    [update] formからtodoをサーバーに登録できる

--- Controllers/todoController.js
+++ Controllers/todoController.js
@@ -5,17 +5,35 @@
         }
     }
 
+    $('#submit-form').submit(function (event) {
+        event.preventDefault();
+        console.log('fire')
+        var todo = $('#submit-form [name=todo]').val();
+        console.log('<_addDataToView>todo: ', todo)
+        global.todo.controller.createTodo(todo)
+    });
+
     TodoController.prototype.init = function() {
+        console.log('init')
         global.todo.model.getTodos(_addDataToView)
+    }
+
+    TodoController.prototype.createTodo = function(todo) {
+        global.todo.model.createTodo(todo, _addDataToView)
     }
 
     global.todo.controller = new TodoController()
 
     function _addDataToView(data){
-        global.todo.model.todos = data
-        for(var i=0; i<data.length; i++){
-            var todo = data[i]
-            global.todo.view.appendTodo(todo)
+        console.log('<_addDataToView>data: ', data)
+        if(data instanceof Array){
+            for(var i=0; i<data.length; i++){
+                var todo = data[i]
+                global.todo.view.appendTodo(todo)
+            }
+        }else{
+            console.log('data: ', data)
+            global.todo.view.appendTodo(data)
         }
     }
 
--- Models/Todo.js
+++ Models/Todo.js
@@ -1,36 +1,49 @@
-(function(global) {
-    var Todo = function(todos) {
-        if(!(this instanceof Todo)) {
+(function (global) {
+    var Todo = function (todos) {
+        if (!(this instanceof Todo)) {
             return new Todo();
         }
 
         this.todos = todos || {}
     }
-    
-    /**
-     * todo一覧を取得
-     */
-    Todo.prototype.getTodos = function(callback) {
+
+    // todo一覧を取得
+    Todo.prototype.getTodos = function (callback) {
         $.ajax({
             type: "GET",
             url: "https://json-now-ohjoczewvz.now.sh/todos"
         })
         .done((data) => {
+            global.todo.model.todos = data
             callback(data)
-            return 1
         }).fail((err) => {
             alert('Todo一覧取得に失敗しました')
-            return 0
-        });
+        })
     }
 
-    Todo.prototype.createTodo = function(task, isDone = false) {
-
+    // todoを作成
+    Todo.prototype.createTodo = function (task, callback) {
+        $.ajax({
+                type: "POST",
+                url: "https://json-now-ohjoczewvz.now.sh/todos",
+                        data: {
+                            'task': task,
+                            isDone: false
+                        }
+            })
+            .done((data) => {
+                global.todo.model.todos = data
+                callback(data)
+            }).fail((err) => {
+                alert('Todo一覧取得に失敗しました')
+            })
     }
 
-    Todo.prototype.updateTodo = function(id) {
+    Todo.prototype.updateTodo = function (id) {
+        console.log('<updateTodo>id: ', id)
+        return id
     }
 
     global.todo.model = new Todo()
 
-  }(window || global)); // ここのglobalはthidでもいいかも. windowがないとき(nodejs)でも動かせる
+}(window || global)); // ここのglobalはthidでもいいかも. windowがないとき(nodejs)でも動かせる
